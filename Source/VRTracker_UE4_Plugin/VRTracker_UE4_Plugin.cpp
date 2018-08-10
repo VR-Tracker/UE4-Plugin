f4d5983f54d5ba7b8b4b9789e90284fd00b1d5de
Jules Thuillier <thuillierjules@gmail.com>
2018-08-10 09:32:07 -0400

    remove everything to switch to UE4.20

--- Source/VRTracker_UE4_Plugin/VRTracker_UE4_Plugin.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "VRTracker_UE4_Plugin.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRTracker_UE4_Plugin, "VRTracker_UE4_Plugin" );
--- Source/VRTracker_UE4_Plugin/VRTracker_UE4_PluginGameModeBase.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "VRTracker_UE4_Plugin.h"
-#include "VRTracker_UE4_PluginGameModeBase.h"
-
-
-
-
